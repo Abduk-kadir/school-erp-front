e25a5ced000b9085488d15788dcdd1b89661898e
Abduk-kadir <ak8871639@gmail.com>
2026-07-23 18:28:34 +0530

    after changes in git again push

--- src/masterLayout/MasterLayout.jsx
+++ src/masterLayout/MasterLayout.jsx
@@ -624,7 +624,7 @@ const MasterLayout = () => {
                           {staff?.firstname}
                         </h6>
                         <span className='text-secondary-light fw-medium text-sm'>
-                          Admin
+                          {staff?.designationInfo?.designation_name}
                         </span>
                       </div>
                       <button type='button' className='hover-text-danger'>
@@ -647,30 +647,7 @@ const MasterLayout = () => {
                           My Profile
                         </Link>
                       </li>
-                      <li>
-                        <Link
-                          className='dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-primary d-flex align-items-center gap-3'
-                          to='/email'
-                        >
-                          <Icon
-                            icon='tabler:message-check'
-                            className='icon text-xl'
-                          />{" "}
-                          Inbox
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          className='dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-primary d-flex align-items-center gap-3'
-                          to='/company'
-                        >
-                          <Icon
-                            icon='icon-park-outline:setting-two'
-                            className='icon text-xl'
-                          />
-                          Setting
-                        </Link>
-                      </li>
+                      
                       <li>
                         <Link
                           className='dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-danger d-flex align-items-center gap-3'
