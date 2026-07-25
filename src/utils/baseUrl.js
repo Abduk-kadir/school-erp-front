99afe801e5d879ae4dd32f90bcf5f4c63b297fb7
Abduk-kadir <ak8871639@gmail.com>
2026-07-25 13:05:30 +0530

    baseurl change

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 //const baseURL = "https://new-test-api.dds-erp.com";
-const baseURL="https://jtsc.dds-erp.com/"
+const baseURL="https://jtsc.dds-erp.com"
 //const baseURL="https://jtsc-cbse.dds-erp.com"
 //const baseURL = "http://localhost:5000";
 
